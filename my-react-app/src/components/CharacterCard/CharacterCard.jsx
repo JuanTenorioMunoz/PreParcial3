0e27d8d0edb656187d11492568a09098b3ce0a6c
JuanTenorioMunoz <tenorio2003@gmail.com>
2025-06-03 21:09:50 -0500

    .

--- my-react-app/src/components/CharacterCard/CharacterCard.jsx
+++ my-react-app/src/components/CharacterCard/CharacterCard.jsx
@@ -1,9 +1,25 @@
+import { useDispatch } from "react-redux"
+
 const CharacterCard = ({name, species, image}) => {
+
+    const dispatch = useDispatch()
+
+    const onEditClick = () => {
+        console.log("eidt")
+    }
+
+    const onDeleteClick = () => {
+        console.log("delete")
+    }
+
+
     return(
         <>
             <h1>{name}</h1>
             <h2>{species}</h2>
             <img src={image}></img>
+            <button onClick={() => onEditClick()}>Edit</button>
+            <button onClick={() => onDeleteClick()}>delete</button>
         </>
     )
 }
